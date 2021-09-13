167694ecce4152f48632a8453628b449ed736009
Jake-C00k <cook.jake@awatapu.school.nz>
2021-09-13 13:06:16 +1200

    Variation 1

---
+++ Name-Age-Color_Var_01.py
@@ -0,0 +1,22 @@
+colors = ["red", "blue", "green", "yellow", "purple", "orange", "pink"]
+
+while True:
+    name = input("What is your name?\n")
+    if str.isalpha(name) == True:
+        break
+    else:
+        print("Please enter a name using the letters A to Z")
+
+while True:
+    try:
+        age = int(input("How old are you?\n"))
+        if 1 <= age <= 118:
+            break
+    except ValueError:
+        print("I don’t recognize that number sorry...")
+while True:
+    fav_color = input("What is your favourite color?\n")
+    if fav_color in colors:
+        break
+    else:
+        print("I’m sorry, I don’t think I’ve heard of that colour, could you try another one?")
