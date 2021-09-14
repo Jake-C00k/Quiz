9675827f6496c2c34cee573eebdbb9b225c626ec
Jake-C00k <cook.jake@awatapu.school.nz>
2021-09-14 14:31:29 +1200

    Variation 1

--- Name-Age-Color/Name-Age-Color_Var_02.py
+++ Name-Age-Color/Name-Age-Color_Var_02.py
@@ -1,29 +1,54 @@
 colors = ["red", "blue", "green", "yellow", "purple", "orange", "pink", "brown"]
 response = ["Ok!", "Right", "Sounds about right"]
 
-while True:
-    name = input("What is your name?\n")
-    if str.isalpha(name) == True:
-        print("That's a nice name!")
-        break
-    else:
-        print("Please enter a name using the letters A to Z")
+yes = ["y", "yes", "yep", "affirmative", "yea"]
+no = ["n", "no", "nope", "negative", "nah"]
 
-while True:
-    try:
-        age = int(input("How old are you?\n"))
-        if 1 <= age <= 118:
-            print("Right!")
-             break
+
+def yes_no(question):
+    answer = None
+    answer = input(question).strip().lower()
+    if answer in yes:
+        yes_no.answer = True
+    elif answer in no:
+        yes_no.answer = False
+    else:
+        print("Please enter Yes or No\n")
+        yes_no(question)
+
+def name_age_color():
+    while True:
+        while True:
+            name = input("What is your name?\n")
+            if str.isalpha(name) == True:
+                print("That's a nice name!")
+                break
+            else:
+                print("Please enter a name using the letters A to Z")
+
+        while True:
+            try:
+                age = int(input("How old are you?\n"))
+                if 1 <= age <= 118:
+                    print("Right!")
+                    break
+                else:
+                    print("I don't think you could be that old...")
+            except ValueError:
+                print("I don’t recognize that number sorry...")
+
+        while True:
+            fav_color = input("What is your favourite color?\n").strip().lower()
+            if fav_color in colors:
+                print("Mine too!")
+                break
+            else:
+                print("I’m sorry, I don’t think I’ve heard of that colour, could you try another one?")
+        yes_no("So your name is {}, you are {} year\s old and your favourite color is {}, right?\n"
+                .format(name, age, fav_color))
+        if yes_no.answer == True:
+            break
         else:
-            print("I don't think you could be that old...")
-    except ValueError:
-        print("I don’t recognize that number sorry...")
+            print("Ok, we'll try again then")
 
-while True:
-    fav_color = input("What is your favourite color?\n").strip().lower()
-    if fav_color in colors:
-        print("Mine too!")
-        break
-    else:
-        print("I’m sorry, I don’t think I’ve heard of that colour, could you try another one?")
+name_age_color()
