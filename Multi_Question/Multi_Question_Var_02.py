20cb55b6c062c33762de5b7b68430b7bd849fa1c
Jake-C00k <cook.jake@awatapu.school.nz>
2021-09-17 10:44:59 +1200

    Variation 2

--- Multi_Question/Multi_Question_Var_02.py
+++ Multi_Question/Multi_Question_Var_02.py
@@ -3,29 +3,28 @@ q1 = ["Which number is highest?\n", "1: fifty-three\n", "2: four", "3: twelve\n"
 
 score = 0
 def multi_question(list, score):
-    while True
+    while True:
         try:
             print(list[0] + list[1] + list[2])
             answer = int(input(list[3]))
-            while True:
-                if 1 <= answer <= 3 :
-                    if answer == list[4]:
-                        print("Correct, {} was the right answer".format(answer))
-                        multi_question.correct = True
-                        break
-                    else:
-                        print("Incorrect, {} was unfortunately the wrong response. Better luck next time".format(answer))
-                        multi_question.correct = False
-                        break
-
+            if 1 <= answer <= 3 :
+                if answer == list[4]:
+                    print("Correct, {} was the right answer".format(answer))
+                    multi_question.correct = True
+                    break
                 else:
-                    print("I’m sorry, but I don’t think there was an option number {}".format(answer))
+                    print("Incorrect, {} was unfortunately the wrong response. Better luck next time".format(answer))
+                    multi_question.correct = False
+                    break
+
+            else:
+                print("I’m sorry, but I don’t think there was an option number {}".format(answer))
         except ValueError:
             print("I’m sorry, I didn’t recognize that input. Could you try again, remembering to only use numbers?")
 
-while 0 <= score < 2:
-    multi_question(q1, score)
-    if multi_question.correct == True:
-        score = 0
-    elif multi_question.correct == False:
+#while 0 <= score < 2:
+multi_question(q1, score)
+if multi_question.correct == True:
+    score = 0
+elif multi_question.correct == False:
         score += 1
