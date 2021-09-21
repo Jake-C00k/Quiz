f577634f2cbea3220b11bdd7fe1b6c71705f3966
Jake-C00k <cook.jake@awatapu.school.nz>
2021-09-21 13:08:34 +1200

    Fin

--- Instructions.py
+++ Instructions.py
@@ -1,11 +1,8 @@
 def instructions():
    input("The program will give you a variety of different questions, most of these multi-choice questions, "
           "but some will be short answer questions.")
-
    input("If the question is multi-choice, please type the corresponding number to the answer you wish to give.")
-
    input("If the question is a short answer, please follow the on-screen instructions.")
-
    input("If you get 1 question wrong, you will lose the quiz.")
 
    print("However, if you get all the way to the end, you win!")
