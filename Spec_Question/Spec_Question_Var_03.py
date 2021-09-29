38a08b692588c8efa0faf61f3c1e3963eb7856a8
Jake-C00k <cook.jake@awatapu.school.nz>
2021-09-30 09:05:41 +1300

    Var

---
+++ Spec_Question/Spec_Question_Var_03.py
@@ -0,0 +1,19 @@
+age = 10
+
+ans = (age * 3) + 14
+
+while True:
+    try:
+        num_response = int(input("What is ({} x 3) + 14?\n".format(age)))
+        if num_response == ans:
+            print("Correct, {} was the right answer".format(num_response))
+            spec_response = True
+            break
+        elif num_response == ans - 1 or num_response == ans + 1:
+            print("So close, {} was unfortunately the wrong response. We’ll give you another shot".format(num_response))
+        else:
+            print("Incorrect, {} was unfortunately the wrong response. Better luck next time".format(num_response))
+            spec_response = False
+            break
+    except ValueError:
+        print("I’m sorry, I didn’t recognize that input. Could you try again, remembering to only use numbers?")
