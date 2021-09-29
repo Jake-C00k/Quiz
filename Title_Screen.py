86d43ac4f1790bc2029cafcd3cb1adc8ff38abbf
Jake-C00k <cook.jake@awatapu.school.nz>
2021-09-30 09:46:28 +1300

    Title Screen Finished

--- Title_Screen.py
+++ Title_Screen.py
@@ -1,3 +1,25 @@
+#Special codes
+#purple
+p = '\033[95m'
+#cyan
+c = '\033[96m'
+#darkcyan
+dc = '\033[36m'
+#blue
+b = '\033[94m'
+#green
+g = '\033[92m'
+#yellow
+y = '\033[93m'
+#red
+r = '\033[91m'
+#bold
+bf = '\033[1m'
+#underline
+uf = '\033[4m'
+#end
+ef = '\033[0m'
+
 def display():
     print(r +"-------------------------------------\n"
       "-----------" + b + "Q" + r +"---"+ b + "U" + r + "---"+ b + "I"+ r + "---"+ b + "Z" + r +
