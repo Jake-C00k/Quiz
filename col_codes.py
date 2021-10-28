02ebc9c39789a7b5ee5b1af806d3d1fa4d91bc17
Jake-C00k <cook.jake@awatapu.school.nz>
2021-10-29 10:23:45 +1300

    Added color codes

---
+++ col_codes.py
@@ -0,0 +1,25 @@
+# Special color codes and text changing effects.
+# purple
+p = '\033[95m'
+# cyan
+c = '\033[96m'
+# darkcyan
+dc = '\033[36m'
+# blue
+b = '\033[94m'
+# green
+g = '\033[92m'
+# yellow
+y = '\033[93m'
+# red
+r = '\033[91m'
+# bold
+bf = '\033[1m'
+# underline
+uf = '\033[4m'
+# end
+ef = '\033[0m'
+#Special text inputs
+confetti = b + "🎉" + r + "--"
+logo = p + "QUIZ FIESTA" + b
+star = r + "*" + y + "*" + g + "*" + b
